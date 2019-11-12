b7926419cdf4d22b22d7758cac3ceed2503c21ac
Marius Krabset <marius.krabset@gmail.com>
2019-11-12 22:51:43 +0100

    clockadapter + 7seg outreg eeprom

---
+++ server/src/main/java/ivark/diycomputer/instructionset/eeprom/SevenSegmentOutregEEPROM.java
@@ -0,0 +1,92 @@
+package ivark.diycomputer.instructionset.eeprom;
+
+import java.io.File;
+import java.io.FileWriter;
+import java.io.PrintWriter;
+
+/**
+ * Created by ivark on 18.10.17.
+ */
+public class SevenSegmentOutregEEPROM {
+
+    static final int[] data={0x3f,0x06,0x5b,0x4f,0x66,0x6d,0x7d,0x07,0x7f,0x6f,0x77,0x7c,0x39,0x5e,0x79,0x71};
+    static final int MODULO=16;
+
+    static public void main(String...args) throws Exception{
+
+        for (int i=0;i<data.length;i++) {
+            data[i]=flip(data[i]);
+            System.out.println("#"+i+"-> 0x"+toHex(data[i],2));
+        }
+        
+        StringBuilder pw=new StringBuilder();
+
+        for (int h=0;h<16;h++) {
+            pw.append("w "+toHex(h*16+768,4));
+            for (int l=0;l<16;l++) {
+                int val=h*16+l;
+                pw.append(" "+toHex(data[val % MODULO],2));
+            }
+            pw.append("\n");
+        }
+
+        for (int h=0;h<16;h++) {
+            pw.append("w "+toHex(h*16+512,4));
+            for (int l=0;l<16;l++) {
+                int val=h*16+l;
+                pw.append(" "+toHex(data[(val/MODULO) % MODULO],2));
+            }
+            pw.append("\n");
+        }
+
+        for (int h=0;h<16;h++) {
+            pw.append("w "+toHex(h*16+256,4));
+            for (int l=0;l<16;l++) {
+                int val=h*16+l;
+                pw.append(" "+toHex(data[(val/(MODULO*MODULO)) % MODULO],2));
+            }
+            pw.append("\n");
+        }
+
+        for (int h=0;h<16;h++) {
+            pw.append("w "+toHex(h*16,4));
+            for (int l=0;l<16;l++) {
+                int val=h*16+l;
+                pw.append(" "+toHex(data[(val/(MODULO*MODULO*MODULO)) % MODULO],2));
+            }
+            pw.append("\n");
+        }
+        System.out.println(pw.toString());
+        writeSpec(pw.toString());
+    }
+
+    public static int flip(int val) {
+        int result=0;
+        int a=0x01;
+        int b=0x80;
+        for (int i=0;i<8;i++) {
+            if ((val & a) == a) {
+                result|=b;
+            }
+            a<<=1;
+            b>>=1;
+        }
+        return result;
+    }
+
+    private static String toHex(Integer v, int length) {
+        String padded = "0000000" + Integer.toHexString(v);
+        return padded.substring(padded.length() - length);
+    }
+
+    private static void writeSpec(String spec) throws Exception {
+        File tempFile = File.createTempFile("eeprom", "7seg");
+        try (PrintWriter pw=new PrintWriter(new FileWriter(tempFile))) {
+            pw.print(spec);
+            pw.flush();
+        }
+        try (SerialWriter sw=new SerialWriter()) {
+            sw.writeToSerial(tempFile);
+        }
+    }
+}
---
+++ v2/circuitboards/docs/clock/README.txt
@@ -0,0 +1,4 @@
+An adjustable 555 based clock for running the computer.
+
+Frequencies between <1hz to >50khz
+
